8bdd079c23626a8bbc080dc90a3851a62b02cbd3
MaPhiVo <68129420+Chipuse@users.noreply.github.com>
2026-05-31 18:42:45 +0200

    * Introduced our Scaffold to main activity

--- app/src/main/java/com/example/team_koeln_bonn/MainActivity.kt
+++ app/src/main/java/com/example/team_koeln_bonn/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.team_koeln_bonn.composables.OurBottomBar
+import com.example.team_koeln_bonn.composables.OurTopBar
+import com.example.team_koeln_bonn.ui.screens.MenuScreen
+import com.example.team_koeln_bonn.ui.screens.SosScreen
 import com.example.team_koeln_bonn.ui.theme.Team_Koeln_BonnTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +30,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Team_Koeln_BonnTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android Phone",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(
+                    modifier = Modifier,
+                    //top app bar content
+                    topBar = {
+                        OurTopBar()
+                    },
+                    floatingActionButton = {
+
+                    },
+                    content = {
+                            paddingValues ->
+                        Box(
+                            modifier = Modifier
+                                .padding(paddingValues)
+                                .fillMaxSize()
+                                .consumeWindowInsets(paddingValues)
+                        ) {
+                            SosScreen()
+                        }
+                    },
+                    bottomBar = {
+                        OurBottomBar()
+                    }
+                )
+
             }
         }
     }
